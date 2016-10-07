6a8ab2ca3635393619eace3dfde21053a5e11c43
Sean McGowan <(none)>
2016-10-07 19:57:11 -0400

    Refactor integration test

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -11,31 +11,31 @@ describe file('/etc/haproxy') do
   it { should be_mode 755 }
 end
 
-instance = {
-  'haproxy' => {
-    'frontends' => {
-      'main' => {
-        'ip' => '*',
-        'port' => '5000',
-        'default_backend' => 'default-backend'
+instances = {
+  haproxy: {
+    frontends: {
+      main: {
+        ip: '*',
+        port: 5000,
+        default_backend: 'default-backend'
       }
     }
   },
-  'haproxy-test' => {
-    'frontends' => {
-      'main' => {
-        'ip' => '*',
-        'port' => '6000',
-        'default_backend' => 'test-backend'
+  :'haproxy-test' => {
+    frontends: {
+      main: {
+        ip: '*',
+        port: 6000,
+        default_backend: 'test-backend'
       }
     }
   }
 }
 
-instance.keys.each do |s|
-  config = "#{s}.cfg"
+instances.each do |i, i_hash|
+  config = "#{i}.cfg"
 
-  describe file("/var/lib/#{s}") do
+  describe file("/var/lib/#{i}") do
     it { should be_directory }
     it { should be_owned_by 'haproxy' }
     it { should be_grouped_into 'haproxy' }
@@ -47,37 +47,37 @@ instance.keys.each do |s|
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
     it { should be_mode 644 }
-    its(:content) { should match %r{^\s*chroot\s+/var/lib/#{Regexp.quote(s)}} }
-    its(:content) { should match %r{^\s*pidfile\s+/var/run/#{Regexp.quote(s)}.pid} }
-    its(:content) { should match %r{^\s*stats socket\s+/var/lib/#{Regexp.quote(s)}/stats} }
+    its(:content) { should match %r{^\s*chroot\s+/var/lib/#{Regexp.quote(i)}} }
+    its(:content) { should match %r{^\s*pidfile\s+/var/run/#{Regexp.quote(i)}.pid} }
+    its(:content) { should match %r{^\s*stats socket\s+/var/lib/#{Regexp.quote(i)}/stats} }
   end
 
-  instance[s]['frontends'].each do |fe, details|
-    socket = "#{details['ip']}:#{details['port']}"
+  i_hash[:frontends].each do |fe, fe_hash|
+    socket = "#{fe_hash[:ip]}:#{fe_hash[:port]}"
 
     describe file("/etc/haproxy/#{config}") do
       its(:content) { should match(/^\s*frontend\s+#{Regexp.quote(fe)}\s+#{Regexp.quote(socket)}$/) }
-      its(:content) { should match(/^\s*default_backend\s+#{Regexp.quote(details['default_backend'])}$/) }
+      its(:content) { should match(/^\s*default_backend\s+#{Regexp.quote(fe_hash[:default_backend])}$/) }
     end
 
-    describe port(details['port']) do
+    describe port(fe_hash[:port]) do
       it { should be_listening.with('tcp') }
     end
   end
 
-  describe file("/etc/init.d/#{s}") do
+  describe file("/etc/init.d/#{i}") do
     it { should be_file }
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
     it { should be_mode 755 }
     its(:content) { should match %r{^cfgfile=/etc/haproxy/#{Regexp.quote(config)}} }
-    its(:content) { should match %r{^pidfile=/var/run/#{Regexp.quote(s)}\.pid} }
-    its(:content) { should match %r{^lockfile=/var/lock/subsys/#{Regexp.quote(s)}} }
-    its(:content) { should match(/^prog=#{Regexp.quote(s)}/) }
+    its(:content) { should match %r{^pidfile=/var/run/#{Regexp.quote(i)}\.pid} }
+    its(:content) { should match %r{^lockfile=/var/lock/subsys/#{Regexp.quote(i)}} }
+    its(:content) { should match(/^prog=#{Regexp.quote(i)}/) }
     its(:content) { should match(/daemon --pidfile=\$pidfile \$exec -D -f \$cfgfile -p \$pidfile \$OPTIONS/) }
   end
 
-  describe service(s) do
+  describe service(i) do
     it { should be_enabled }
     it { should be_running }
   end
