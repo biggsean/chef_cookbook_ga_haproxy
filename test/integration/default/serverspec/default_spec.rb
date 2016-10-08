89f867c61ab40549c62725e002d5319365ec3341
Sean McGowan <(none)>
2016-10-07 21:57:11 -0400

    Add backend configuration tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -10,7 +10,36 @@ describe file('/etc/haproxy') do
   it { should be_grouped_into 'root' }
   it { should be_mode 755 }
 end
-
+backends = {
+  :'default-backend' => {
+    servers: [
+      app1: {
+        socket: '127.0.0.1:5001',
+        options: ['check']
+      },
+      app2: {
+        socket: '127.0.0.1:5002',
+        options: ['check']
+      },
+      app3: {
+        socket: '127.0.0.1:5003',
+        options: ['check']
+      },
+      app4: {
+        socket: '127.0.0.1:5004',
+        options: ['check']
+      }
+    ]
+  },
+  :'http-backend' => {
+    servers: [
+      app1: {
+        socket: '127.0.0.1:5001',
+        options: ['check']
+      }
+    ]
+  }
+}
 instances = {
   haproxy: {
     frontends: {
@@ -22,9 +51,10 @@ instances = {
       http: {
         ip: '*',
         port: 80,
-        default_backend: 'default_backend'
+        default_backend: 'http-backend'
       }
-    }
+    },
+    backends: backends
   },
   :'haproxy-test' => {
     frontends: {
@@ -33,7 +63,8 @@ instances = {
         port: 6000,
         default_backend: 'test-backend'
       }
-    }
+    },
+    backends: backends
   }
 }
 
@@ -67,6 +98,20 @@ instances.each do |i, i_hash|
 
     describe port(fe_hash[:port]) do
       it { should be_listening.with('tcp') }
+    end
+  end
+
+  i_hash[:backends].each do |be, be_hash|
+    describe file("/etc/haproxy/#{config}") do
+      its(:content) { should match(/^\s*backend\s+#{Regexp.quote(be)}$/) }
+
+      be_hash[:servers].each do |server|
+        server.each do |s_name, s_info|
+          its(:content) do
+            should match(/^\s*server\s+#{Regexp.quote(s_name)}\s+#{Regexp.quote(s_info[:socket])}\s#{Regexp.quote(s_info[:options].join(' '))}$/)
+          end
+        end
+      end
     end
   end
 
