a504f3dc5d3535ed5a8f3413f9f4259a9f539e04
Sean McGowan <spmcgowan@gmail.com>
2016-10-09 16:36:29 -0400

    Seperate instance specific tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -14,23 +14,6 @@ end
 instances.each do |i, i_hash|
   config = "#{i}.cfg"
 
-  describe file("/var/lib/#{i}") do
-    it { should be_directory }
-    it { should be_owned_by 'haproxy' }
-    it { should be_grouped_into 'haproxy' }
-    it { should be_mode 755 }
-  end
-
-  describe file("/etc/haproxy/#{config}") do
-    it { should be_file }
-    it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
-    it { should be_mode 644 }
-    its(:content) { should match %r{^\s*chroot\s+/var/lib/#{Regexp.quote(i)}} }
-    its(:content) { should match %r{^\s*pidfile\s+/var/run/#{Regexp.quote(i)}.pid} }
-    its(:content) { should match %r{^\s*stats socket\s+/var/lib/#{Regexp.quote(i)}/stats} }
-  end
-
   i_hash[:frontends].each do |fe, fe_hash|
     socket = "#{fe_hash[:ip]}:#{fe_hash[:port]}"
 
@@ -62,22 +45,5 @@ instances.each do |i, i_hash|
         end
       end
     end
-  end
-
-  describe file("/etc/init.d/#{i}") do
-    it { should be_file }
-    it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
-    it { should be_mode 755 }
-    its(:content) { should match %r{^cfgfile=/etc/haproxy/#{Regexp.quote(config)}} }
-    its(:content) { should match %r{^pidfile=/var/run/#{Regexp.quote(i)}\.pid} }
-    its(:content) { should match %r{^lockfile=/var/lock/subsys/#{Regexp.quote(i)}} }
-    its(:content) { should match(/^prog=#{Regexp.quote(i)}/) }
-    its(:content) { should match(/daemon --pidfile=\$pidfile \$exec -D -f \$cfgfile -p \$pidfile \$OPTIONS/) }
-  end
-
-  describe service(i) do
-    it { should be_enabled }
-    it { should be_running }
   end
 end
---
+++ test/integration/default/serverspec/instance_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+instances.keys.each do |instance|
+  config = "/etc/haproxy/#{instance}.cfg"
+
+  describe file("/etc/init.d/#{instance}") do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 755 }
+    its(:content) { should match(/^cfgfile=#{Regexp.quote(config)}/) }
+    its(:content) { should match %r{^pidfile=/var/run/#{Regexp.quote(instance)}\.pid} }
+    its(:content) { should match %r{^lockfile=/var/lock/subsys/#{Regexp.quote(instance)}} }
+    its(:content) { should match(/^prog=#{Regexp.quote(instance)}/) }
+    its(:content) { should match(/daemon --pidfile=\$pidfile \$exec -D -f \$cfgfile -p \$pidfile \$OPTIONS/) }
+  end
+
+  describe file("/var/lib/#{instance}") do
+    it { should be_directory }
+    it { should be_owned_by 'haproxy' }
+    it { should be_grouped_into 'haproxy' }
+    it { should be_mode 755 }
+  end
+
+  describe file(config) do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 644 }
+    its(:content) { should match %r{^\s*chroot\s+/var/lib/#{Regexp.quote(instance)}} }
+    its(:content) { should match %r{^\s*pidfile\s+/var/run/#{Regexp.quote(instance)}.pid} }
+    its(:content) { should match %r{^\s*stats socket\s+/var/lib/#{Regexp.quote(instance)}/stats} }
+  end
+
+  describe service(instance) do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
