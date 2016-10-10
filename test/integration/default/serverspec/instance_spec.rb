a621c80403bb17b708a6ce60bbf79e24dca93d8e
Sean McGowan <spmcgowan@gmail.com>
2016-10-10 13:27:58 -0400

    Ensure directories are correct for each instance

--- test/integration/default/serverspec/instance_spec.rb
+++ test/integration/default/serverspec/instance_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 instances.keys.each do |instance|
-  config = "/etc/haproxy/#{instance}.cfg"
+  cfgdir = '/etc/haproxy'
+  config = "#{cfgdir}/#{instance}.cfg"
+  dotd = "#{cfgdir}/#{instance}.d"
 
   describe file("/etc/init.d/#{instance}") do
     it { should be_file }
@@ -30,6 +32,22 @@ instances.keys.each do |instance|
     its(:content) { should match %r{^\s*chroot\s+/var/lib/#{Regexp.quote(instance)}} }
     its(:content) { should match %r{^\s*pidfile\s+/var/run/#{Regexp.quote(instance)}.pid} }
     its(:content) { should match %r{^\s*stats socket\s+/var/lib/#{Regexp.quote(instance)}/stats} }
+  end
+
+  describe file(dotd) do
+    it { should be_directory }
+    it { should be_owned_by 'haproxy' }
+    it { should be_grouped_into 'haproxy' }
+    it { should be_mode 755 }
+  end
+
+  %w(frontends backends).each do |dir|
+    describe file("#{dotd}/#{dir}") do
+      it { should be_directory }
+      it { should be_owned_by 'haproxy' }
+      it { should be_grouped_into 'haproxy' }
+      it { should be_mode 755 }
+    end
   end
 
   describe service(instance) do
