b8d90a0ccf4c42bb8a03e581b9ad982437933a29
Sean McGowan <spmcgowan@gmail.com>
2016-10-12 19:48:29 -0400

    Fix style issues

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -2,36 +2,36 @@
 module GAHAProxyCookbook
   # Helper functions
   module Helpers
+    # configuration class
     class ResourceConfig
-      PACKAGE_NAME = 'haproxy'
-      PIDFILE = '/var/run/%s.pid'
-      CHROOTDIR = '/var/lib/%s'
-      SERVICEFILE = '/etc/init.d/%s'
-      BASEDIR = '/etc/haproxy'
-      CFG = BASEDIR + '/%s.cfg'
-      DOTDIR = BASEDIR + '/%s.d'
-      FEDIR = DOTDIR + '/frontends'
-      BEDIR = DOTDIR + '/backends'
-      FELINKDIR = FEDIR + '/enabled'
-      BELINKDIR = BEDIR + '/enabled'
-
-      FECFG = FEDIR + '/%s.cfg'
-      BECFG = BEDIR + '/%s.cfg'
-      FELINK = FEDIR + '/enabled/%s.cfg'
-      BELINK = BEDIR + '/enabled/%s.cfg'
+      PACKAGE_NAME = 'haproxy'.freeze
+      PIDFILE = '/var/run/%s.pid'.freeze
+      CHROOTDIR = '/var/lib/%s'.freeze
+      SERVICEFILE = '/etc/init.d/%s'.freeze
+      BASEDIR = '/etc/haproxy'.freeze
+      CFG = BASEDIR + '/%s.cfg'.freeze
+      DOTDIR = BASEDIR + '/%s.d'.freeze
+      FEDIR = DOTDIR + '/frontends'.freeze
+      BEDIR = DOTDIR + '/backends'.freeze
+      FELINKDIR = FEDIR + '/enabled'.freeze
+      BELINKDIR = BEDIR + '/enabled'.freeze
+      FECFG = FEDIR + '/%s.cfg'.freeze
+      BECFG = BEDIR + '/%s.cfg'.freeze
+      FELINK = FEDIR + '/enabled/%s.cfg'.freeze
+      BELINK = BEDIR + '/enabled/%s.cfg'.freeze
 
       attr_reader :package_name, :basedir, :pidfile, :chrootdir,
-        :servicefile, :cfg, :dotdir, :fedir, :bedir, :felinkdir,
-        :belinkdir, :fecfg, :becfg, :felink, :belink, :name
+                  :servicefile, :cfg, :dotdir, :fedir, :bedir,
+                  :felinkdir, :belinkdir, :fecfg, :becfg, :felink,
+                  :belink, :name
 
-      def initialize(instance_name, options ={})
-        @name = set_instance_name(instance_name)
+      def initialize(instance_name, options = {})
+        @name = format_instance_name(instance_name)
         @package_name = PACKAGE_NAME
         @basedir = BASEDIR
-        set_instance_specific_vars
-        if options[:endtype] && options[:endname]
-          set_end_specific_vars(options)
-        end
+        make_instance_specific_vars
+        return unless options[:endtype] && options[:endname]
+        make_end_specific_vars(options)
       end
 
       def config_dirs
@@ -40,7 +40,7 @@ module GAHAProxyCookbook
 
       private
 
-      def set_instance_name(instance_name)
+      def format_instance_name(instance_name)
         case instance_name
         when 'default'
           'haproxy'
@@ -48,15 +48,15 @@ module GAHAProxyCookbook
         end
       end
 
-      def set_instance_specific_vars
+      def make_instance_specific_vars
         vars = %w(pidfile chrootdir servicefile cfg dotdir fedir bedir felinkdir belinkdir)
         vals = [PIDFILE, CHROOTDIR, SERVICEFILE, CFG, DOTDIR, FEDIR, BEDIR, FELINKDIR, BELINKDIR]
         vars.zip(vals).each do |var, val|
-          instance_variable_set("@#{var}", sprintf(val, @name))
+          instance_variable_set("@#{var}", format(val, @name))
         end
       end
 
-      def set_end_specific_vars(options)
+      def make_end_specific_vars(options)
         if options[:endname]
           case options[:endtype]
           when :frontend
@@ -69,7 +69,7 @@ module GAHAProxyCookbook
 
       def create_end_vars(vars, vals, endname)
         vars.zip(vals).each do |var, val|
-          instance_variable_set("@#{var}", sprintf(val, @name, endname))
+          instance_variable_set("@#{var}", format(val, @name, endname))
         end
       end
     end
