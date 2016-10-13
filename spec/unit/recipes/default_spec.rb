8a737355b8104c988e23e9a0b7230c789bb5a9f3
Sean McGowan <spmcgowan@gmail.com>
2016-10-13 10:50:27 -0400

    Fix bugs in chefspec

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -29,52 +29,59 @@ describe 'ga_haproxy_default::default' do
       expect(chef_run).to enable_ga_haproxy_frontend('main').with(
         instance_name: 'default',
         socket: '*:5000',
-        default_backend: 'default-backend'
+        default_backend: 'main-backend'
       )
       expect(chef_run).to enable_ga_haproxy_frontend('http').with(
         instance_name: 'default',
         socket: '*:80',
-        default_backend: 'test-backend'
+        default_backend: 'http-backend'
       )
     end
 
     it 'enables main and https frontend for haproxy test instance' do
-      expect(chef_run).to enable_ga_haproxy_frontend('main').with(
+      expect(chef_run).to enable_ga_haproxy_frontend('test-main').with(
         instance_name: 'test',
         socket: '*:6000',
-        default_backend: 'test-backend'
+        default_backend: 'test-main-backend'
       )
       expect(chef_run).to enable_ga_haproxy_frontend('https').with(
         instance_name: 'test',
         socket: '*:443',
-        default_backend: 'test-backend'
+        default_backend: 'https-backend'
       )
     end
 
-    %w(default test).each do |instance|
-      it "enables default-backend and test-backend for #{instance}" do
-        expect(chef_run).to enable_ga_haproxy_backend('default-backend').with(
+    %w(default test).zip(%w(main-backend test-main-backend)).each do |instance, backend|
+      it "enables #{backend} for #{instance}" do
+        expect(chef_run).to enable_ga_haproxy_backend(backend).with(
           instance_name: instance,
           servers: [
-            { name: 'app1', socket: '127.0.0.1:5001', options: ['check'] },
-            { name: 'app2', socket: '127.0.0.1:5002', options: ['check'] },
-            { name: 'app3', socket: '127.0.0.1:5003', options: ['check'] },
-            { name: 'app4', socket: '127.0.0.1:5004', options: ['check'] }
+            { 'name' => 'app1', 'socket' => '127.0.0.1:5001', 'options' => ['check'] },
+            { 'name' => 'app2', 'socket' => '127.0.0.1:5002', 'options' => ['check'] },
+            { 'name' => 'app3', 'socket' => '127.0.0.1:5003', 'options' => ['check'] },
+            { 'name' => 'app4', 'socket' => '127.0.0.1:5004', 'options' => ['check'] }
           ],
           options: [
             'balance roundrobin',
             'option httpchk HEAD / HTTP/1.1\r\nHost:localhost'
           ]
         )
-        expect(chef_run).to enable_ga_haproxy_backend('test-backend').with(
-          servers: [{ name: 'app1', socket: '127.0.0.1:5001', options: ['check'] }]
+      end
+    end
+    %w(default test).zip(%w(http-backend https-backend)).each do |instance, backend|
+      it "enables #{backend} for #{instance}" do
+        expect(chef_run).to enable_ga_haproxy_backend(backend).with(
+          instance_name: instance,
+          servers: [
+            { 'name' => 'app1', 'socket' => '127.0.0.1:5001', 'options' => ['check'] }
+          ]
         )
       end
     end
 
-    it 'disables https frontend and default-backend for test instance' do
-      expect(chef_run).to disable_ga_haproxy_frontend('https').with(instance_name: test)
-      expect(chef_run).to disable_ga_haproxy_backend('default-backend').with(instance_name: test)
+    it 'disables https frontend and https-backend for test instance' do
+      expect(chef_run).to disable_ga_haproxy_frontend('https').with(instance_name: 'test')
+      expect(chef_run).to disable_ga_haproxy_backend('https-backend').with(instance_name: 'test')
     end
   end
 end
