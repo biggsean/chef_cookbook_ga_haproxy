53dc82395085708982830e8642b152323fe843e3
Sean McGowan <spmcgowan@gmail.com>
2016-10-13 11:18:25 -0400

    Add description of tests in default spec

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
-describe package('haproxy') do
-  it { should be_installed }
-end
+describe 'Ensure the package install is successful' do
+  describe package('haproxy') do
+    it { should be_installed }
+  end
 
-describe file('/etc/haproxy') do
-  it { should be_directory }
-  it { should be_owned_by 'root' }
-  it { should be_grouped_into 'root' }
-  it { should be_mode 755 }
+  describe file('/etc/haproxy') do
+    it { should be_directory }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 755 }
+  end
 end
